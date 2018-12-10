14a61dc8b735cb419c74448dcfcb22768e6cdcc0
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-12-10 14:31:20 -0500

    convert_dcm2nii: First commit

---
+++ scripts/convert_dcm2nii.py
@@ -0,0 +1,63 @@
+import os
+import argparse
+import shutil
+
+
+def get_parameters():
+    parser = argparse.ArgumentParser(description='This script is used to export unf_sct spineGeneric in BIDS')
+    parser.add_argument("-d", "--data",
+                        help="Path to data directory",
+                        required=True)
+    args = parser.parse_args()
+    return args
+
+def main(path_data):
+    """
+    Main function
+    :param path_data:
+    :return:
+    """
+    contrast_dict = {'T1w': ('T1w', 'anat'), 
+                'T2w': ('T2w', 'anat'),
+                'DWI': ('dwi', 'dwi'),
+                'GRE-MT0': ('acq-MToff_MTS','anat'),
+                'GRE-MT1': ('acq-MTon_MTS','anat'),
+                'GRE-T1w': ('acq-T1w_MTS','anat'),
+                'GRE-ME': ('T2star','anat'),}
+   
+    keylist_contrast = contrast_dict.keys()
+
+    file_extension_dict = {'nii': 'nii.gz', 'json': 'json', 'bval': 'bval', 'bvec': 'bvec'}
+    # Convert dcm to nii
+    for dirName, subdirList, fileList in os.walk(path_data):
+        for file in fileList:
+            if file.startswith(file.split('-')[0] + '-0001.dcm'):
+                init_path = os.path.join(dirName,file)
+                os.system("dcm2niix -b y -z y " + init_path ) 
+    print '\n'
+
+    # Loop across all files+folder within the subject's folder
+    # TODO: just loop across files instead of fetching all files+folder
+    for dirName, subdirList, fileList in os.walk(path_data):
+        # Loop across all files in the local dir
+        for file in fileList:
+            if file.split('.')[1] in file_extension_dict :
+                init_path = os.path.join(dirName,file)
+                file_extension = file_extension_dict[file.split('.')[1]]
+                current_contrast = file.split('_')[1]              
+
+                # If file name includes contrast listed in dictionary, then rename and move in BIDS dir
+                if current_contrast in contrast_dict:
+                    root_new_path = '/'.join(dirName.split('/')[0:(len(dirName.split('/'))-2)])
+
+                    #Create BIDS folder structure
+                    bids_folder_path = os.path.join(root_new_path,'bids_'+ (dirName.split('/')[(len(dirName.split('/'))-2)]),'sub-01',contrast_dict[current_contrast][1])
+                    bids_file_path = os.path.join(bids_folder_path, 'sub-01_' + contrast_dict[current_contrast][0] + '.' + file_extension)
+                    if not os.path.exists(bids_folder_path):
+                        os.makedirs(bids_folder_path)
+
+                    os.rename(init_path,bids_file_path)
+
+if __name__ == "__main__":
+    args = get_parameters()
+    main(args.data)
