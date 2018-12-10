46171df0a83b44253521b287c67d428378bc1923
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-12-10 16:02:33 -0500

    convert_dcm2nii.py: Added flag for subject and path out

--- scripts/convert_dcm2nii.py
+++ scripts/convert_dcm2nii.py
@@ -19,48 +19,46 @@ def get_parameters():
     parser = argparse.ArgumentParser(description='Convert DICOM data to NIFTI and organize into BIDS structure. The '
                                                  'BIDS structure is specific to the spine_generic project. More info at: '
                                                  'https://github.com/sct-pipeline/spine_generic')
-    parser.add_argument("-d", "--data",
-                        help="Path to DICOM directory",
+    parser.add_argument('-d', '--path-dicom',
+                        help='Path to input DICOM directory.',
                         required=True)
+    parser.add_argument('-s', '--subject',
+                        help='Subject number (e.g. sub-03). Required by BIDS to name folders and files.',
+                        required=True)
+    parser.add_argument('-o', '--path-output',
+                        help='Path to output NIFTI files sorted into BIDS directory. Default is current directory.',
+                        required=False)
     args = parser.parse_args()
     return args
 
-def main(path_data):
+
+def convert_dcm2nii(path_data, subject, path_out='./'):
     """
-    Main function
-    :param path_data:
+    Convert DICOM data to BIDS-compatible NIFTI files.
+    :param path_data: Path to input DICOM directory
+    :param subject: Subject number (e.g. sub-03). Required by BIDS to name folders and files
+    :param path_out: Path to output NIFTI files sorted into BIDS directory
     :return:
     """
-    path_out = './'
 
+    # Dictionary of BIDS naming. First element: file name suffix, Second element: destination folder.
     contrast_dict = {
-        'GRE-MT0': ('acq-MToff_MTS','anat'),
-        'GRE-MT1': ('acq-MTon_MTS','anat'),
-        'GRE-T1w': ('acq-T1w_MTS','anat'),
-        'GRE-ME': ('T2star','anat'),
+        'GRE-MT0': ('acq-MToff_MTS', 'anat'),
+        'GRE-MT1': ('acq-MTon_MTS', 'anat'),
+        'GRE-T1w': ('acq-T1w_MTS', 'anat'),
+        'GRE-ME': ('T2star', 'anat'),
         'T1w': ('T1w', 'anat'),
         'T2w': ('T2w', 'anat'),
         'DWI': ('dwi', 'dwi'),
     }
-   
-    keylist_contrast = contrast_dict.keys()
-
-    file_extension_dict = {'nii': 'nii.gz', 'json': 'json', 'bval': 'bval', 'bvec': 'bvec'}
-
-    # Go to folder
-    os.chdir(path_data)
 
     # Convert dcm to nii
-    os.system('dcm2niix -b y -z y -x n -v y ' + path_out)
-
-    # for dirName, subdirList, fileList in os.walk(path_data):
-    #     for file in fileList:
-    #         if file.startswith(file.split('-')[0] + '-0001.dcm'):
-    #             init_path = os.path.join(dirName,file)
-    #             os.system("dcm2niix -b y -z y " + init_path )
-    # print '\n'
+    os.makedirs(path_out, exist_ok=True)
+    # os.chdir(path_data)
+    os.system('dcm2niix -b y -z y -x n -v y -o ' + path_out + ' ' + path_data)
 
     # Loop across NIFTI files
+    os.chdir(path_out)
     nii_files = glob.glob('*.nii.gz')
     for nii_file in nii_files:
         # Loop across contrasts
@@ -79,27 +77,7 @@ def main(path_data):
                     shutil.move(nii_file_all_ext, fname_out)
                 break
 
-    # TODO: just loop across files instead of fetching all files+folder
-    # for dirName, subdirList, fileList in os.walk(path_data):
-    #     # Loop across all files in the local dir
-    #     for file in fileList:
-    #         if file.split('.')[1] in file_extension_dict :
-    #             init_path = os.path.join(dirName,file)
-    #             file_extension = file_extension_dict[file.split('.')[1]]
-    #             current_contrast = file.split('_')[1]
-    #
-    #             # If file name includes contrast listed in dictionary, then rename and move in BIDS dir
-    #             if current_contrast in contrast_dict:
-    #                 root_new_path = '/'.join(dirName.split('/')[0:(len(dirName.split('/'))-2)])
-    #
-    #                 #Create BIDS folder structure
-    #                 bids_folder_path = os.path.join(root_new_path,'bids_'+ (dirName.split('/')[(len(dirName.split('/'))-2)]),'sub-01',contrast_dict[current_contrast][1])
-    #                 bids_file_path = os.path.join(bids_folder_path, 'sub-01_' + contrast_dict[current_contrast][0] + '.' + file_extension)
-    #                 if not os.path.exists(bids_folder_path):
-    #                     os.makedirs(bids_folder_path)
-    #
-    #                 os.rename(init_path,bids_file_path)
 
 if __name__ == "__main__":
     args = get_parameters()
-    main(args.data)
+    convert_dcm2nii(args.path_dicom, args.subject, path_out=args.path_output)
